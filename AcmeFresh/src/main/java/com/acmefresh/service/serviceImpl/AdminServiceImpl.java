55ca204775ae4aab48ad530f72e9d8c0d9516fa5
Rupesh Thakur <101567722+Rupesh8844@users.noreply.github.com>
2023-01-26 10:55:32 +0530

    Update AdminServiceImpl.java

--- AcmeFresh/src/main/java/com/acmefresh/service/serviceImpl/AdminServiceImpl.java
+++ AcmeFresh/src/main/java/com/acmefresh/service/serviceImpl/AdminServiceImpl.java
@@ -63,5 +63,123 @@ public class AdminServiceImpl implements AdminService{
 	
 
 	
+	@Override
+	public Admin updateAdmin(Admin admin, String Username, String key) {
+		
+		Optional<AdminSession> otp = adminSessionDao.findByUuid(key);
+
+		Admin updatedAdmin = null;
+		
+		if (otp.isEmpty()) {
+			throw new LoginException("Admin is not logged in, Please login first!");
+		}
+		else {
+			Optional<Admin> opt = adminDao.findByUsername(Username);
+			if (opt.isEmpty()) {
+				throw new UsernameNotFoundException("Username not found.");
+			}
+			else {
+				Admin toUpdate = opt.get();
+				updatedAdmin = adminDao.save(toUpdate);
+			}
+		}
+		return updatedAdmin;
+	}
+	
+	
+
+	
+	
+	@Override
+	public String addProducts(AcmeFreshHydroponicProduce produce, String key) {
+		
+		Optional<AdminSession> otp = adminSessionDao.findByUuid(key);
+		
+		if (otp.isEmpty()) {
+			throw new LoginException("Admin is not logged in, Please login first!");
+		}
+		productDao.save(produce);
+		return "Product has been added Successfully...";
+	}
+
+	
+	
+	@Override
+	public String updateProduct(AcmeFreshHydroponicProduce produce, String key) {
+		
+		Optional<AdminSession> otp = adminSessionDao.findByUuid(key);
+		
+		if (otp.isEmpty()) {
+			throw new LoginException("Admin is not logged in, Please login first!");
+		}
+		
+		Optional<AcmeFreshHydroponicProduce> opt=productDao.findById(produce.getProductId());
+		
+		if(opt.isEmpty()) {
+			throw new LoginException("Product not found with this Id..");
+		}
+		
+		
+		productDao.save(produce);
+		return "Product have been updated Successfully...";
+	}
+
+	@Override
+	public String updatePriceOfProductById(Integer id, Double newPrice, String key) {
+		
+		Optional<AdminSession> otp = adminSessionDao.findByUuid(key);
+		
+		if (otp.isEmpty()) {
+			throw new LoginException("Admin is not logged in, Please login first!");
+		}
+		
+		Optional<AcmeFreshHydroponicProduce> opt=productDao.findById(id);
+		
+		if(opt.isEmpty()) {
+			throw new LoginException("Product not found with this Id..");
+		}
+		
+		AcmeFreshHydroponicProduce old =opt.get();
+		old.setPrice(newPrice);
+		productDao.save(old);
+		return "Price have been updated Successfully...";
+		
+	}
+	
+		@Override
+	public String deleteByUsername(LoginDTO dto, String key) {
+		
+		Optional<AdminSession> otp = adminSessionDao.findByUuid(key);
+		
+		if (otp.isEmpty()) {
+			throw new LoginException("Admin is not logged in, Please login first!");
+		}
+		else {
+			Optional<Admin> opt = adminDao.findByUsername(dto.getUsername());
+			if (opt.isEmpty()) {
+				throw new UsernameNotFoundException("Username not found");
+			}
+			else {
+				Admin admin = opt.get();
+				adminDao.delete(admin);
+			}
+		}
+		return "Id with username "+dto.getUsername()+"is deletd Successfully...";
+	}
+	
+	
+
+	@Override
+	public String logoutAdmin(String key) throws LoginException {
+		
+		Optional<AdminSession> otp = adminSessionDao.findByUuid(key);
+		
+		if (otp.isEmpty()) {
+			throw new LoginException("Admin is not logged in, Please login first!");
+		}
+
+		adminSessionDao.delete(otp.get());
+		return "Admin has been Successfully logged out.";
+	}
 
 }
